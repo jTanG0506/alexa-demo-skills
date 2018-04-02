436eddd154753929670bd700cd88065b869f12a1
Jonathan Tang <jonathan.tang@student.manchester.ac.uk>
2018-04-02 22:59:42 +0100

    Implement handler for 'AlexaMeetupCityCheck'
    Tells the user whether or not the requested city has an Alexa meetup

--- Skills/voice-devs/src/index.js
+++ Skills/voice-devs/src/index.js
@@ -20,5 +20,35 @@ var handlers = {
   'AlexaMeetupNumbers': function() {
     var meetupNumbers = alexaMeetups.length;
     this.emit(':ask', `I currently know of ${meetupNumbers} Alexa developer meetups. Check to see if your city is one of them!`, 'How can I help?');
+  },
+  'AlexaMeetupCityCheck': function() {
+    // Obtain slot values
+    var USCitySlot = this.event.request.intent.slots.USCity.value;
+    var EuropeanCitySlot = this.event.request.intent.slots.EuropeanCity.value;
+
+    // Check which city we actually have
+    var city;
+    if (USCitySlot) {
+      city = USCitySlot;
+    } else if (EuropeanCitySlot) {
+      city = EuropeanCitySlot;
+    } else {
+      this.emit(':ask', 'Sorry, I didn\'t recognise that city name.', 'How can I help?');
+    }
+
+    // Check whether city has a meetup
+    var cityMatch = '';
+    for (var i = 0; i < alexaMeetups.length; i++) {
+      if (city.toLowerCase() === alexaMeetups[i].city.toLowerCase()) {
+        cityMatch = alexaMeetups[i].city;
+      }
+    }
+
+    // Respond to user
+    if (cityMatch !== '') {
+      this.emit(':ask', `Yes! ${city} has an Alexa meetup!`, 'How can I help?');
+    } else {
+      this.emit(':ask', `Sorry, looks like ${city} doesn't have an Alexa developer meetup yet - why don't you start one?`, 'How can I help?');
+    }
   }
 };
