c3d589b1cf84ea414029b5815ad99d921fa649ec
Jonathan Tang <jonathan.tang@student.manchester.ac.uk>
2018-04-02 13:38:26 +0100

    Add 'skeleton-code' template

---
+++ Skills/skeleton-code/src/index.js
@@ -0,0 +1,16 @@
+// Boilerplate code for the lambda function for an Alexa Skill in ASK.
+
+var Alexa = require('alexa-sdk');
+
+exports.handler = function(event, context, callback){
+  var alexa = Alexa.handler(event, context);
+  alexa.registerHandlers(handlers);
+  alexa.execute();
+};
+
+// Define handler functions below.
+var handlers = {
+  'LaunchRequest': function () {
+    this.emit(':ask', 'Welcome to my template code!', 'Try saying hello!');
+  }
+};
