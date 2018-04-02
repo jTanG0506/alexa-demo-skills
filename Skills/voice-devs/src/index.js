cfe85d5d2677accef578e789e5a0c4835df2a0ab
Jonathan Tang <jonathan.tang@student.manchester.ac.uk>
2018-04-02 22:12:21 +0100

    Implement handler for 'AlexaMeetupNumbers'
    Returns the number of events in the sample Alexa meetup data

--- Skills/voice-devs/src/index.js
+++ Skills/voice-devs/src/index.js
@@ -1,6 +1,7 @@
-// Boilerplate code for the lambda function for an Alexa Skill in ASK.
-
 var Alexa = require('alexa-sdk');
+
+// Include data file
+var alexaMeetups = require('./data/alexaMeetups');
 
 exports.handler = function(event, context, callback){
   var alexa = Alexa.handler(event, context);
@@ -15,5 +16,9 @@ var handlers = {
   },
   'HelloIntent': function() {
     this.emit(':tell', 'Hi there!');
+  },
+  'AlexaMeetupNumbers': function() {
+    var meetupNumbers = alexaMeetups.length;
+    this.emit(':ask', `I currently know of ${meetupNumbers} Alexa developer meetups. Check to see if your city is one of them!`, 'How can I help?');
   }
 };
