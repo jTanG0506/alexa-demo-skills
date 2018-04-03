652eb5979c7d3d6ed2f9c6e1ec517458835b761a
Jonathan Tang <jonathan.tang@student.manchester.ac.uk>
2018-04-03 19:24:33 +0100

    Implement handler for 'CountryCapture'

--- Skills/voice-devs/src/index.js
+++ Skills/voice-devs/src/index.js
@@ -14,9 +14,11 @@ exports.handler = function(event, context, callback){
 
 // Define handler functions below.
 var handlers = {
+
   'NewSession': function() {
     this.emit(':ask', 'Welcome to Voice Devs, the skill that gives you information about the alexa developer community. You can ask me about the various alexa meetups around the world, or listen to the Alexa Dev Chat podcast. But first, I\'d like to get to know you better. Tell me your name by saying: My name is, and then your name.', 'Tell me your name by saying: My name is, and then your name.');
   },
+
   'NameCapture': function() {
     // Obtain slot values
     var USFirstNameSlot = this.event.request.intent.slots.USFirstName.value;
@@ -38,10 +40,28 @@ var handlers = {
       this.emit(':ask', 'Sorry, I didn\'t recognise that name. Please tell me your name by saying: My name is, and then your name.', 'Please tell me your name by saying: My name is, and then your name.');
     }
   },
+
+  'CountryCapture': function() {
+    // Obtain slot values
+    var country = this.event.request.intent.slots.Country.value;
+
+    // Get user name from session attributes
+    var userName = this.attributes['userName'];
+
+    // Save country name to session attributes then ask for favourite language
+    if (country) {
+      this.attributes['userCountry'] = country;
+      this.emit(':ask', `Ok, ${userName}! You're from ${country}, that's great! You can ask me about the various alexa meetups around the world, or listen to the Alexa Dev postcast. What would you like to do?`, 'What would you like to do?');
+    } else {
+      this.emit(':ask', 'Sorry, I didn\'t recognise that country. Please tell me what country you\'re from by saying: I am from, and then the country you\'re from.', 'Please tell me what country you\'re from by saying: I am from, and then the country you\'re from.');
+    }
+  },
+
   'AlexaMeetupNumbers': function() {
     var meetupNumbers = alexaMeetups.length;
     this.emit(':ask', `I currently know of ${meetupNumbers} Alexa developer meetups. Check to see if your city is one of them!`, 'How can I help?');
   },
+
   'AlexaMeetupCityCheck': function() {
     // Obtain slot values
     var USCitySlot = this.event.request.intent.slots.USCity.value;
@@ -78,6 +98,7 @@ var handlers = {
       this.emit(':ask', `Sorry, looks like ${city} doesn't have an Alexa developer meetup yet - why don't you start one?`, 'How can I help?');
     }
   },
+
   'AlexaMeetupOrganiserCheck': function() {
     // Obtain slot values
     var USCitySlot = this.event.request.intent.slots.USCity.value;
