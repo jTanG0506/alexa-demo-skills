2086b13973ad202276f14d0f79259e57022368a4
Jonathan Tang <jonathan.tang@student.manchester.ac.uk>
2018-04-04 10:33:07 +0100

    Change Onboarding handlers to use MeetupAPI information
    Ask users to link account if there is no access token

--- Skills/voice-devs/src/handlers/onboardingStateHandlers.js
+++ Skills/voice-devs/src/handlers/onboardingStateHandlers.js
@@ -2,6 +2,8 @@ var Alexa = require('alexa-sdk');
 
 var constants = require('../constants/constants');
 
+var meetupAPI = require('../helpers/meetupAPI');
+
 var onboardingStateHandlers = Alexa.CreateStateHandler(constants.states.ONBOARDING, {
 
   'NewSession': function() {
@@ -12,7 +14,31 @@ var onboardingStateHandlers = Alexa.CreateStateHandler(constants.states.ONBOARDI
       this.handler.state = constants.states.MAIN;
       this.emitWithState('LaunchRequest');
     } else {
-      this.emit(':ask', 'Welcome to Voice Devs, the skill that gives you information about the alexa developer community. You can ask me about the various alexa meetups around the world, or listen to the Alexa Dev Chat podcast. But first, I\'d like to get to know you better. Tell me your name by saying: My name is, and then your name.', 'Tell me your name by saying: My name is, and then your name.');
+      // Get access token
+      var accessToken = this.event.session.user.accessToken;
+
+      // Check if account is linked or not.
+      if (accessToken) {
+        meetupAPI.GetUserDetails(accessToken)
+        .then((userDetails) => {
+          // Get the users name and store it in the session.
+          var name = userDetails.name;
+          this.attributes['userName'] = name;
+
+          // Change state to MAIN
+          this.handler.state = constants.state.MAIN;
+
+          // Welcome user for the first time
+          this.emit(':ask', `Hi ${name}! Welcome to Voice Devs! The skill that gives you all the information about the Alexa developer community. You can ask me about the various Alexa meetups around the world, or listen to the Alexa Dev Chat podcast. What would you like to do?`, 'What would you like to do?');
+        })
+        .catch((error) => {
+          // API error.
+          console.log("MeetupAPI Error: ", error);
+          this.emit(':tell', 'Sorry, there was a problem accessing your meetup account details.');
+        });
+      } else {
+        this.emit(':tellWithLinkAccountCard', 'Please link your account to use this skill. I\'ve sent the details to your Alexa app');
+      }
     }
   },
 
@@ -32,14 +58,8 @@ var onboardingStateHandlers = Alexa.CreateStateHandler(constants.states.ONBOARDI
   },
 
   'AMAZON.HelpIntent': function() {
-    // Username attribute check
-    var userName = this.attributes['userName'];
-
-    if (userName) {
-      this.emit(':ask', 'Please tell me what country you\'re from by saying: I am from, and then the country you\'re from.', 'Tell me what country you\'re from by saying: I am from, and then the country you\'re from.');
-    } else {
-      this.emit(':ask', 'Please tell me what country you\'re from by saying: I am from, and then the country you\'re from.', 'Tell me what country you\'re from by saying: I am from, and then the country you\'re from.');
-    }
+    // Help in this state means account is not linked.
+    this.emit(':tellWithLinkAccountCard', 'Please link your account to use this skill. I\'ve sent the details to your Alexa app');
   },
 
   'Unhandled': function() {
